8ca8c6c92d7d474b9910b1e1333542cb77e8d234
ricmaps <ricardomapurungajunior@gmail.com>
2026-03-12 16:33:08 -0300

    refactor not very secure

--- not-very-secure.c
+++ not-very-secure.c
@@ -1,13 +1,11 @@
+#include <ctype.h>
 #include <stddef.h>
 #include <stdbool.h>
 
 bool alphanumeric(const char *str_in) {
   if(*str_in == NULL) return false;
   for(; *str_in != NULL; str_in++) {
-    if(*str_in >= '0' && *str_in <= '9' ||
-       *str_in >= 'A' && *str_in <= 'Z' ||
-       *str_in >= 'a' && *str_in <= 'z') 
-      continue;
+    if(isalnum(*str_in)) continue;
     return false;
   }
   return true;
